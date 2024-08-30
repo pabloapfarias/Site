0938316d3fb668520adec7b52efced0ad834bf26
Pablo Farias <contato@pablofarias.com.br>
2024-08-30 14:06:42 -0300

    Inicio de estudos

---
+++ src/componentes/Quadrado.tsx
@@ -0,0 +1,8 @@
+const Quadrado = () => {
+    return (
+        <div className="w-52 h-52 bg-amber-700 text-slate-100">
+            Texto do componente quadrado
+        </div>
+    );
+}
+export default Quadrado;
